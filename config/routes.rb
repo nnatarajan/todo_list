244f5af35ffade2fbe92bcd2811e20c3c17e32c9
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-20 23:09:35 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  # get 'todo_items/index'
+
+  # get 'todo_items/new'
+
+  # get 'todo_items/show'
+
+  # get 'todo_items/edit'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todo_items
+end
