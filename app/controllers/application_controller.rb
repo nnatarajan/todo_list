9a85ae0b3822478222f24956da5f160c6d794528
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-22 00:23:29 -0600

    updated ux

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def welcome
-    render html: "Welcome! Ready to create a To Do List? "
+    render html: "Welcome! Ready to create a To Do List?"
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    redirect_to @todos = Todo.all
   end
 
   def help
   end
+
 end
