399a345da0181af5f1cfc1b45b465fce9f1baa8b
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-21 22:33:55 -0600

    U of CRUD, prevented the creation of  blank todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,27 +1,47 @@
 class TodosController < ApplicationController
+before_action :set_todo, only: [:show, :edit, :update, :destroy]
   def index
     @todos = Todo.all
+  end
+
+  def show
   end
 
   def new
     @todo = Todo.new
   end
 
+  def edit
+  end
+
   def create
-    new_params = params.require(:todo).permit(:name)
     @todo = Todo.new(new_params)
       if @todo.save
         redirect_to @todo
       else
-        render 'new'
+        render :new  #'new'
       end
   end
 
-  def show
-    @todo = Todo.find(params[:id])
+    def update
+    if @todo.update(new_params)
+      redirect_to @todo
+    else
+      render :edit
+    end
   end
 
-  def edit
-    @todo = Todo.find(params[:id])
+  def destroy
+    @todo.destroy
+    redirect_to @todo
   end
+
+  private
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    def new_params
+      params.require(:todo).permit(:name)
+    end
 end
